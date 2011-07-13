3fee058c699c39da47f9bf70ae0de36423f71fa3
risk <technoweenie@gmail.com>
2011-07-13 13:08:09 -0700

    Release 0.3.0

--- lib/running_man.rb
+++ lib/running_man.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
 
 module RunningMan
-  VERSION = '0.2.1'
+  VERSION = '0.3.0'
 
   # Public: Sets up any helper class methods in TestClassMethods on the 
   # specified test case class.
